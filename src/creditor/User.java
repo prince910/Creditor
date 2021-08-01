b782f373c35cee77101a2382ca9a74c4d167f86f
prince910 <pcbv910@gmail.com>
2021-08-02 05:17:10 +0800

    Initialization of git with add user feature

---
+++ src/creditor/User.java
@@ -0,0 +1,105 @@
+package creditor;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author prince.c.b.vergara
+ */
+public class User {
+    private int userId;
+    private String mobileNumber;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String email;
+
+    /**
+     * @return the userId
+     */
+    public int getUserId() {
+        return userId;
+    }
+
+    /**
+     * @param userId the userId to set
+     */
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * @return the mobileNumber
+     */
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    /**
+     * @param mobileNumber the mobileNumber to set
+     */
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the midddleName
+     */
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    /**
+     * @param midddleName the midddleName to set
+     */
+    public void setMiddleName(String midddleName) {
+        this.middleName = midddleName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
